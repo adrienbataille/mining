614623c2c2f30fe74ff625e26d090ed6c8d8ee73
Adrien Bataille <abataille@turbulent.ca>
2018-03-17 21:30:45 -0400

    Changed tests

--- __tests__/models/mining/MinerStatisticsRecord.jest.js
+++ __tests__/models/mining/MinerStatisticsRecord.jest.js
@@ -1,20 +1,44 @@
-import MinerStatistics from '../../../src/models/miner/MinerStatistics.ts';
-import mockMinerStatistics from './mocks/minerStatistics.json';
+import MinerStatisticsRecord from '../../../src/models/mining/MinerStatisticsRecord';
+import { minerStatisticsParser } from '../../../src/parsers/mining/minerStatisticsParser';
+import mockMinerStatistics from './mocks/minerStatisticsMock';
 
 describe('Miner Statistics', () => {
   it('can instanciate the model', () => {
-    const model = new MinerStatistics();
+    const model = new MinerStatisticsRecord();
 
     expect(model).not.toBeFalsy();
+    expect(model.miner).toBeDefined();
+    expect(model.activeWorkers).toBeDefined();
+    expect(model.unpaid).toBeDefined();
+
+    expect(model.statistics).toBeDefined();
+    expect(model.statistics.time).toBeDefined();
+    expect(model.statistics.lastSeen).toBeDefined();
+    expect(model.statistics.reportedHashrate).toBeDefined();
+    expect(model.statistics.currentHashrate).toBeDefined();
+    expect(model.statistics.averageHashrate).toBeDefined();
+    expect(model.statistics.validShares).toBeDefined();
+    expect(model.statistics.invalidShares).toBeDefined();
+    expect(model.statistics.staleShares).toBeDefined();
   });
 
   it('can instanciate the model from data', () => {
     const { data } = mockMinerStatistics;
-    const model = new MinerStatistics(data);
+    const model = new MinerStatisticsRecord(minerStatisticsParser(data));
 
     expect(model).not.toBeFalsy();
-    expect(model.reportedHashrate).toBeDefined();
-    expect(model.currentHashrate).toBeDefined();
-    expect(model.averageHashrate).toBeDefined();
+    expect(model.miner).toBeDefined();
+    expect(model.activeWorkers).toBeDefined();
+    expect(model.unpaid).toBeDefined();
+
+    expect(model.statistics).toBeDefined();
+    expect(model.statistics.time).toBeDefined();
+    expect(model.statistics.lastSeen).toBeDefined();
+    expect(model.statistics.reportedHashrate).toBeDefined();
+    expect(model.statistics.currentHashrate).toBeDefined();
+    expect(model.statistics.averageHashrate).toBeDefined();
+    expect(model.statistics.validShares).toBeDefined();
+    expect(model.statistics.invalidShares).toBeDefined();
+    expect(model.statistics.staleShares).toBeDefined();
   });
 });
--- __tests__/models/mining/WorkerStatisticsRecord.jest.js
+++ __tests__/models/mining/WorkerStatisticsRecord.jest.js
@@ -1,24 +1,42 @@
-import WorkerStatistics from '../../../src/models/worker/WorkerStatistics.ts';
-import mockWorkerStatistics from './mocks/workerStatistics.json';
+import WorkerStatisticsRecord from '../../../src/models/mining/WorkerStatisticsRecord';
+import { workerStatisticsParser } from '../../../src/parsers/mining/workerStatisticsParser';
+import mockWorkerStatistics from './mocks/workerStatisticsMock';
 
 describe('Worker Statistics', () => {
   it('can instanciate the model', () => {
-    const model = new WorkerStatistics();
+    const model = new WorkerStatisticsRecord();
 
     expect(model).not.toBeFalsy();
+    expect(model.miner).toBeDefined();
+    expect(model.worker).toBeDefined();
+
+    expect(model.statistics).toBeDefined();
+    expect(model.statistics.time).toBeDefined();
+    expect(model.statistics.lastSeen).toBeDefined();
+    expect(model.statistics.reportedHashrate).toBeDefined();
+    expect(model.statistics.currentHashrate).toBeDefined();
+    expect(model.statistics.averageHashrate).toBeDefined();
+    expect(model.statistics.validShares).toBeDefined();
+    expect(model.statistics.invalidShares).toBeDefined();
+    expect(model.statistics.staleShares).toBeDefined();
   });
 
   it('can instanciate the model from data', () => {
     const { data } = mockWorkerStatistics;
-    const model = new WorkerStatistics(data);
+    const model = new WorkerStatisticsRecord(workerStatisticsParser(data));
 
     expect(model).not.toBeFalsy();
+    expect(model.miner).toBeDefined();
     expect(model.worker).toBeDefined();
-    expect(model.reportedHashrate).toBeDefined();
-    expect(model.currentHashrate).toBeDefined();
-    expect(model.averageHashrate).toBeDefined();
-    expect(model.validShares).toBeDefined();
-    expect(model.invalidShares).toBeDefined();
-    expect(model.staleShares).toBeDefined();
+
+    expect(model.statistics).toBeDefined();
+    expect(model.statistics.time).toBeDefined();
+    expect(model.statistics.lastSeen).toBeDefined();
+    expect(model.statistics.reportedHashrate).toBeDefined();
+    expect(model.statistics.currentHashrate).toBeDefined();
+    expect(model.statistics.averageHashrate).toBeDefined();
+    expect(model.statistics.validShares).toBeDefined();
+    expect(model.statistics.invalidShares).toBeDefined();
+    expect(model.statistics.staleShares).toBeDefined();
   });
 });
